4819173efb0b035316360c9c9f3bdf91bba611f4
SatyamBhargav <satyambhargav17@gmail.com>
2022-10-01 10:55:37 +0530

    Code updated

--- javaDatatype.java
+++ javaDatatype.java
@@ -2,7 +2,7 @@
  * Question of the day
  * https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
  */
-
+import java.util.*;
 public class javaDatatype {
     public static void main(String[] args) {
 
@@ -17,9 +17,9 @@ public class javaDatatype {
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)
                 System.out.println("* byte");
-                if ( x > = -32768 && x < = 32767 ) System.out.println ( " * short " ) ;
-                if ( x > = -2147483648 && x < = 2147483647 ) System.out.println ( " * int " ) ;
-                if (x >= -9223372036854775808L && x < = 9223372036854775807L ) System.out.println( " * long " ) ;
+                if ( x >= -32768 && x <= 32767 ) System.out.println ( " * short " ) ;
+                if ( x >= -2147483648 && x <= 2147483647 ) System.out.println ( " * int " ) ;
+                if (x >= -9223372036854775808L && x <= 9223372036854775807L ) System.out.println( " * long " ) ;
                 //Complete the code
             }
             catch(Exception e)
@@ -28,5 +28,6 @@ public class javaDatatype {
             }
 
         }
+        sc.close();
     }
 }
