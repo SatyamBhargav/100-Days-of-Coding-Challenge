49416985e15a46b0130868a6cae9894bd772d595
SatyamBhargav <satyambhargav17@gmail.com>
2022-10-03 09:12:11 +0530

    Day 5/100

---
+++ IntroToString.java
@@ -0,0 +1,25 @@
+/*
+ * Question of the day
+ * https://www.hackerrank.com/challenges/java-strings-introduction/problem?isFullScreen=true
+ */
+
+
+import java.util.*;;
+public class IntroToString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(capitalize(A) + " " + capitalize(B));
+    }
+    
+    private static String capitalize(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1, word.length());
+    
+        
+    }
+    
+}
