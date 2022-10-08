93abbb8a642e4227262555465a97ca257d78a650
SatyamBhargav <satyambhargav17@gmail.com>
2022-10-08 15:01:36 +0530

    Day 10/100

---
+++ StringTokens.java
@@ -0,0 +1,23 @@
+/*
+ * Question of the day
+ * https://www.hackerrank.com/challenges/java-string-tokens/problem?isFullScreen=true
+ */
+
+import java.util.*;
+
+public class StirngTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.replaceAll("[ !,?._'@]+", " ");
+        StringTokenizer st = new StringTokenizer(s," ");  
+        System.out.println(st.countTokens());
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+        scan.close();
+        }
+}
+
