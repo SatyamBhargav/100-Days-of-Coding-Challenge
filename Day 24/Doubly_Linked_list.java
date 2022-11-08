471ebc20e0f26c74f2955a925f49ca6163694ce3
SatyamBhargav <satyambhargav17@gmail.com>
2022-11-08 11:06:16 +0530

    Day 24/100

---
+++ Day 24/Doubly_Linked_list.java
@@ -0,0 +1,141 @@
+public class Doubly_Linked_list {
+
+    Node head;
+    Node tail;
+
+
+    public class Node {
+
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value){
+            this.value = value;
+        }
+
+        Node(Node prev, int value, Node next){
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    void insertFirst(int val){
+        Node node = new Node(val);
+        if (head == null) {
+            node.next = head;
+            node.next = tail;
+            node.prev = null;
+            head = node;
+            tail = node;
+            return;
+        }
+
+        if (head != null) {
+            node.next = head;
+            node.prev = null;
+            head.prev = node;
+            head = node;
+        }
+    }
+
+    void insertEnd(int val){
+        Node node = new Node(val);
+        if (tail == null) {
+            insertFirst(val);
+        }
+
+        node.prev = tail;
+        tail.next = node;
+        node.next = null;
+        tail = node;
+    }
+
+    void insert(int val, int index){
+        Node node = new Node(val);
+        Node element = head;
+        for (int i = 1; i < index; i++) {
+            element = element.next;
+        }
+        node.next = element.next;
+        node.prev = element;
+        element.next.prev = node;
+        element.next = node;
+
+        /*
+        bellow code help us to verify our code
+
+        System.out.println(element.value);
+        System.out.println(element.next.value);
+        System.out.println(element.next.prev.value);
+        System.out.println(element.next.next.value);
+        System.out.println(element.next.next.prev.value);
+        */
+    }
+
+    void delete(int val){
+        if (head.value == val) {
+            head = head.next;
+            head.prev = null;
+
+            /*
+            code verifier
+            System.out.println(head.value);
+            System.out.println(head.prev);
+            System.out.println(head.next.value);
+            */
+        }
+
+        if (tail.value == val) {
+            tail = tail.prev;
+            tail.next = null;
+        }
+
+        Node element = head;
+        while (element != null) {
+            if (element.value == val) {
+                element.next.prev = element.prev;
+                element.prev.next = element.next;
+            }
+            element = element.next;
+        } 
+        
+    }
+
+    void display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) {
+        Doubly_Linked_list dll = new Doubly_Linked_list();
+        dll.insertFirst(6);
+        dll.insertFirst(5);
+        dll.insertFirst(10);
+        dll.insertEnd(12);
+        /*
+        for code verification
+
+        System.out.println(dll.head.value);
+        System.out.println(dll.tail.value);
+        System.out.println(dll.head.next.value);
+        System.out.println(dll.tail.next);
+        System.out.println(dll.head.prev);
+        System.out.println(dll.tail.prev.value);
+        */
+        dll.insert(2, 2);
+        dll.display();
+        dll.delete(10);
+        dll.display();
+        dll.delete(12);
+        dll.display();
+        dll.delete(2);
+        dll.display();
+    }
+    
+}
